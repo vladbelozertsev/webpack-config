3e59e6b7eeb50c08155ed3a750dd0e993bc9e718
someuserABC <someuserABC@gmail.com>
2020-10-17 10:28:43 +0300

    restruct

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,6 @@
 // import "bootstrap.native"; // см в loader'ah (bootstrap.native loader) подключаемые скрипты
 import "./assets/js/main";
-import "./assets/scss/main.scss";
+import "./assets/scss/style.scss";
 
 // При таком подключение, стили swiper работают НЕ КОРРЕКТНО, в частности не отображается pagination:
 // import "../../node_modules/swiper/swiper.scss";
